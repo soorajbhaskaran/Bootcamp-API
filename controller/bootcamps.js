cd1cc9f0886ca9ec826a20d244ead316e23987a0
sooraj <sbkaiprath99@gmail.com>
2020-09-20 00:16:10 +0530

    db seeder and finding bootcamp from distance functionality

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -2,6 +2,7 @@
 const Bootcamp = require('../models/Bootcamp');
 const ErrorResponce = require('../utils/errorResponce');
 const asyncHandler = require('../middleware/asyn');
+const geocoder = require('../utils/geocoder');
 
 //@desc get all bootcamps
 //@router /api/v1/bootcamp
@@ -61,5 +62,34 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponce(`Bootcamp not found with id ${req.params.id}`, 404));
     }
     res.status(200).send({ success: 'true', data: {} });
+
+});
+
+//@desc get bootcamps within a radius in a zipcode
+//@router /api/v1/bootcamp/radius/:zipcode/:distance
+//@access Private
+exports.getBootcampFromRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params;
+
+    //get lattitude/longitude from geocoder
+    const loc = await geocoder.geocode(zipcode);
+    const lat = loc[0].latitude;
+    const lng = loc[0].longitude;
+
+    //Calculate radius in radian
+    //distance divided by radius of earth
+    //Radius of earth 6378km
+    const radius = await Bootcamp.create({
+        location: {
+            $geoWithin: { $centerSphere: [[lng, lat], radius] }
+        }
+    });
+
+    //Sending status to client
+    res.status(200).json({
+        success: false,
+        count: bootcamp.length,
+        data: bootcamp
+    })
 
 });
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
-const { getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getSingleBootcamp } = require('../controller/bootcamps');
+const { getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getSingleBootcamp, getBootcampFromRadius } = require('../controller/bootcamps');
 const router = express.Router();
 
+router.route('/radius/:zipcode/:distance').get(getBootcampFromRadius);
 router.route('/').get(getBootcamp).post(createBootcamp);
 router.route('/:id').get(getSingleBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
